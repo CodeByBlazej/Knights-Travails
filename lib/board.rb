408bf2615f3a4fec5fd6bbfb526ce0ac7a97931a
CodeByBlazej <codebyblazej@gmail.com>
2024-12-21 21:30:24 +0000

    add append method

--- lib/board.rb
+++ lib/board.rb
@@ -2,8 +2,14 @@ require_relative "vertex"
 
 class Board
  
-  def initialize
-    @vertex_x = nil
-    @vertex_y = nil
+  def initialize(x, y)
+    @vertex_x = x
+    @vertex_y = y
+  end
+
+  def append(x, y)
+    if @vertex_x.nil?
+      @vertex_x = Vertex.new()
+    end
   end
 end
