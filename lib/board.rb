311fcc6f19817198896eb9344c08ef0bb41bd090
CodeByBlazej <codebyblazej@gmail.com>
2024-12-22 12:20:58 +0000

    add current move and add add vertices method

--- lib/board.rb
+++ lib/board.rb
@@ -1,17 +1,26 @@
 require_relative "vertex"
 
 class Board
-  attr_accessor :graph, :vertex_x, :vertex_y
+  attr_accessor :graph, :current_position, :vertex_x, :vertex_y
 
-  def initialize(x, y)
+  def initialize
+    @vertex_x = nil
+    @vertex_y = nil
+    @current_position = []
+    # @graph = []
+    @graph = Array.new(4) { Array.new(4, 0) }
+
+  end
+
+  def add_vertices(x, y)
     @vertex_x = Vertex.new(x)
     @vertex_y = Vertex.new(y)
-    @graph = [@vertex_x, @vertex_y]
-  end
-
-  def append(x, y)
-    if @vertex_x.nil?
-      @vertex_x = Vertex.new()
-    end
+    @graph << @vertex_x.value
+    @graph << @vertex_y.value
+    @current_position = [x, y]
   end
 end
+
+# make graph using Array.new method to set up the size of the graph.
+# then in knight method make a loop to keep moving knight either 1,2 2,1
+# or -1, -2 or -2, -1
