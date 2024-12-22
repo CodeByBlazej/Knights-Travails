08c75a76d2712a595b9df6af71850a339d9a38f1
CodeByBlazej <codebyblazej@gmail.com>
2024-12-22 19:59:28 +0000

    add possible moves method

--- lib/knight.rb
+++ lib/knight.rb
@@ -10,28 +10,54 @@ class Knight
     if @board.nil?
       @board = Board.new
     end
-    
-    @board.add_vertices(from[0], from[1])
+    #if current position is empty then make new currnt pos regarding whats given
+    #and count every possible move from there and change current possition
+    if @board.current_position.empty?
+      @board.current_position = [from[0], from[1]]
+      @board.graph[from[0]][from[1]] = 1
+    end
 
+    if @board.graph[to[0]][to[1]].nil?
+      # possible_moves(to)?????????
+    else
+      puts "field you want to go to is taken by other chess piece"
+    end
     p @board.current_position
+    p @board.graph
+    # possible_moves(to)
 
-    # until @board.graph.include(to)
+    # @board.add_vertices(from[0], from[1])
+
+    # p @board.current_position
+
+    # # until @board.graph.include(to)
       
+    # # end
+    
+    # if @board.graph.include?(to)
+    #   puts @board.graph
+    # else
+    #   @board.add_vertices(@allowed_move[0], @allowed_move[1])
     # end
     
-    if @board.graph.include?(to)
-      puts @board.graph
-    else
-      @board.add_vertices(@allowed_move[0], @allowed_move[1])
-    end
-    
-    p @board.graph
-    p to
-    p @board.current_position
+    # p @board.graph
+    # p to
+    # p @board.current_position
   end
 
-  def move
-    
+  def possible_moves(to)
+    return if @board.current_position == to
+      
+    temporary_position = []
+
+    if @board.current_position[0] < to[0]
+      temporary_position << @board.current_position[0] + @allowed_move[0]
+      temporary_position << @board.current_position[1] + @allowed_move[1]
+    end
+    p temporary_position
+    @board.graph[0][0] = 1
+    p @board.current_position
+    p @board.graph
   end
 
 end
