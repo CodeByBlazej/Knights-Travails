651d6ad166a262996b04b5dbd660ff5629d5642d
CodeByBlazej <codebyblazej@gmail.com>
2024-12-22 20:01:06 +0000

    add graph to the method

--- lib/board.rb
+++ lib/board.rb
@@ -15,6 +15,7 @@ class Board
   def add_vertices(x, y)
     @vertex_x = Vertex.new(x)
     @vertex_y = Vertex.new(y)
+    @graph[]
     @graph << @vertex_x.value
     @graph << @vertex_y.value
     @current_position = [x, y]
