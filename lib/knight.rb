1fa7a9dc11bfd887a6e6a768e4907192a10e5f25
CodeByBlazej <codebyblazej@gmail.com>
2024-12-22 12:20:37 +0000

    manipulate knight moves method

--- lib/knight.rb
+++ lib/knight.rb
@@ -3,12 +3,35 @@ require_relative "board"
 class Knight
   def initialize
     @allowed_move = [1, 2] || [2, 1]
+    @board = nil
   end
 
   def knight_moves(from, to)
-    if board.graph.nil?
-      
+    if @board.nil?
+      @board = Board.new
     end
+    
+    @board.add_vertices(from[0], from[1])
+
+    p @board.current_position
+
+    # until @board.graph.include(to)
+      
+    # end
+    
+    if @board.graph.include?(to)
+      puts @board.graph
+    else
+      @board.add_vertices(@allowed_move[0], @allowed_move[1])
+    end
+    
+    p @board.graph
+    p to
+    p @board.current_position
+  end
+
+  def move
+    
   end
 
 end
