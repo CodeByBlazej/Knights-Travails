f070c5de28593ef161c9387fe60076e5b47ced73
CodeByBlazej <codebyblazej@gmail.com>
2024-12-26 09:26:45 +0000

    add update graph method

--- lib/knight.rb
+++ lib/knight.rb
@@ -54,14 +54,17 @@ class Knight
     if @tree.find(to) == to
       puts "yeeeaa!"
       @board.current_position = to
+      update_graph(from)
       result << @board.current_position
       return result
     elsif @tree.find(to) != to
       closest_move = @filtered_moves.min_by { |move| (to[0] - move[0]) + (to[1] - move[1]) }
       @board.current_position = closest_move
+      update_graph(from)
       result << @board.current_position
     end
-    
+    p @board.current_position
+    p @board.graph
   end
 
   def make_tree(from)
@@ -80,5 +83,15 @@ class Knight
     @tree.build_tree
     @tree.pretty_print
     p @filtered_moves
+  end
+
+  def update_graph(from)
+    if @board.current_position.empty?
+      @board.current_position = [from[0], from[1]]
+      @board.graph[from[0]][from[1]] = 1
+    else
+      @board.graph[from[0]][from[1]] = 0
+      @board.graph[@board.current_position[0]][@board.current_position[1]] = 1
+    end
   end
 end
