9c30935288c1f5e6dccb42282b4be00bed6c336c
CodeByBlazej <codebyblazej@gmail.com>
2024-12-26 13:00:40 +0000

    solve exercise but knight lunatic close to destination

--- lib/knight.rb
+++ lib/knight.rb
@@ -6,6 +6,7 @@ class Knight
   def initialize
     @allowed_moves = nil
     @filtered_moves = nil
+    @previous_moves = []
     @board = nil
     @tree = nil
   end
@@ -17,7 +18,7 @@ class Knight
     
     update_graph(from)
 
-    p @board.graph
+    # p @board.graph
     possible_moves(from, to)
 
   end
@@ -26,7 +27,7 @@ class Knight
     
     # result = [from]
     return result if @board.current_position == to
-    make_tree(from)
+    make_tree(from, result)
     
     
     if @tree.find(to) == to
@@ -34,12 +35,13 @@ class Knight
       @board.current_position = to
       update_graph(from)
       result << @board.current_position
-      return 
+      return result.inspect
     end
     
     if @tree.find(to) != to
-      binding.pry
-      closest_move = @filtered_moves.min_by { |move| (to[0] - move[0]) + (to[1] - move[1]) }
+      # binding.pry
+      # closest_move = @tree.level_order
+      closest_move = @filtered_moves.min_by { |move| (to[0] - move[0]).abs + (to[1] - move[1]).abs }
       @board.current_position = closest_move
       update_graph(from)
       result << @board.current_position
@@ -48,22 +50,22 @@ class Knight
     possible_moves(@board.current_position, to, result)
   end
 
-  def make_tree(from)
+  def make_tree(from, result)
     moves = [-2, -1, 1, 2]
     @allowed_moves = moves.permutation(2).select { |a, b| a + b != 0 }
     potential_moves = @allowed_moves.map { |a, b| [a + from[0], b + from[1]]}
-    @filtered_moves = potential_moves.select { |a, b| a >= 0 && a <= 7 && b >= 0 && b <= 7} - @board.current_position
-    p @allowed_moves
-    p potential_moves
-    p @filtered_moves
+    @filtered_moves = potential_moves.select { |a, b| a >= 0 && a <= 7 && b >= 0 && b <= 7 && !result.include?([a, b])}
+    # p @allowed_moves
+    # p potential_moves
+    # p @filtered_moves
     
 
     arr_for_tree = @filtered_moves
-    p @filtered_moves
+    # p @filtered_moves
     @tree = Tree.new(arr_for_tree.dup << @board.current_position)
     @tree.build_tree
     @tree.pretty_print
-    p @filtered_moves
+    # p @filtered_moves
   end
 
   def update_graph(from)
