e42c785d9a5e7e7f8e5dda014beb9fda9479c914
CodeByBlazej <codebyblazej@gmail.com>
2024-12-28 12:01:02 +0000

    organize class

--- game.rb
+++ game.rb
@@ -1,20 +1,13 @@
-require_relative "lib/board"
+require_relative "lib/node"
 require_relative "lib/knight"
 
 class Game
-  attr_accessor :knight
 
   def initialize
     @knight = Knight.new()
   end
 
-  # board = Board.new(0, 1)
-  # puts board.graph
-  # puts board.graph[0].value
-  # puts board.graph[1].value
   def play
     @knight.knight_moves([0, 0], [7, 7])
-    
   end
-
 end
